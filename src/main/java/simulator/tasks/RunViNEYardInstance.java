ccd1981d98518d5af255baabf6a419ac5549a10a
embs <embs@cin.ufpe.br>
2014-09-23 01:58:59 -0300

    Introduz task RunViNEYardInstance

---
+++ src/main/java/simulator/tasks/RunViNEYardInstance.java
@@ -0,0 +1,18 @@
+package simulator.tasks;
+
+import java.util.HashMap;
+
+import simulator.io.ViNEYardVNMPReader;
+import simulator.mapping.GraspMapper;
+import simulator.simulation.Simulation;
+
+public class RunViNEYardInstance {
+  public static void main(String[] args) {
+    ViNEYardVNMPReader reader = new ViNEYardVNMPReader(
+      "/home/embs/Code/vine-yard/sub-50.txt",
+      "/home/embs/Code/vine-yard/r-2000-50-20-10-10-10-50");
+    Simulation simulation = new Simulation("vine_yard_instance", reader);
+    GraspMapper mapper = new GraspMapper();
+    simulation.simulate(mapper);
+  }
+}
