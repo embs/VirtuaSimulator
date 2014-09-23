5023f01f51ef3e013df5e0e74e2c85f292afac90
embs <embs@cin.ufpe.br>
2014-09-23 15:09:42 -0300

    Muda task RunViNEYardInstance para usar a mesma instância do problema exibida no artigo de Boutaba

--- src/main/java/simulator/tasks/RunViNEYardInstance.java
+++ src/main/java/simulator/tasks/RunViNEYardInstance.java
@@ -10,7 +10,7 @@ public class RunViNEYardInstance {
   public static void main(String[] args) {
     ViNEYardVNMPReader reader = new ViNEYardVNMPReader(
       "/home/embs/Code/vine-yard/sub-50.txt",
-      "/home/embs/Code/vine-yard/r-2000-50-20-10-10-10-50");
+      "/home/embs/Code/vine-yard/r-2000-50-50-20-10-5-25");
     Simulation simulation = new Simulation("vine_yard_instance", reader);
     GraspMapper mapper = new GraspMapper();
     simulation.simulate(mapper);
